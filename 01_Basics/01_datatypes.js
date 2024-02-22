7ddfc3f64829b1ff9c208bfd430de01d0140fb54
Varshit_17 <91016291+varshitjhawar@users.noreply.github.com>
2024-02-22 12:49:12 +0000

    commit Conversion and datatype

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,18 @@
+//code readability should be high
+
+let name = "Varshit Jhawar"
+let age = 28
+let isloggedI = false
+
+//Number =>2^53
+//bigint
+//string "",''
+//boolean => true,false
+//null => sandalone value
+//undefined => value nott assign
+//symbol => unique
+
+
+//object 
+
+console.log(typeof "Varshit");
