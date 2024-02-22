9068c575221d43621689f15930ccb14d015cc2fa
Varshit_17 <91016291+varshitjhawar@users.noreply.github.com>
2024-02-22 13:06:03 +0000

    conversion

---
+++ 01_Basics/02_conversion.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+//Conversion
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0
+//null=0
+//undefined = NaN
+
+//boolean
+//1 = true; 0 = false
+//"" = false
+// "varshit" = true
+
+
+//*************************************OPERAIONS*******************************************
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let a = 3
+let b = ++a
+console.log(a)
+console.log(b)
